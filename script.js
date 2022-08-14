2e4579f2b8f5d6800a5162c615ef6ea73ee73e99
Mauro <MauroMarchesan88@gmail.com>
2022-08-14 11:21:16 -0300

    Added thumbnails for cart and some css

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ function createProductItemElement({ sku, name, image }) {
 
   return section;
 }
+
 const returnItemInfo = async () => {
   const products = await fetchProducts('computador');
   const productsData = products.results;
@@ -61,6 +62,7 @@ const returnItemInfo = async () => {
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
+
 function cartItemClickListener(event) {
   const item = event.target;
   const itemSplitted = item.innerText.split('$');
@@ -72,25 +74,32 @@ function cartItemClickListener(event) {
   prices(finalPrice, 'subs');
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ sku, name, salePrice, thumbnail }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
 const addItemToCart = async (event) => {
   const chosenItemButton = event.target;
   const buttonParentNode = chosenItemButton.parentElement;
   const brother = buttonParentNode.firstChild;
   const chosenItem = await fetchItem(brother.innerText);
   const finalPrice = Number(chosenItem.price.toFixed(2));
+  const thumbnail = chosenItem.thumbnail;
+  const img = document.createElement('img');
+  img.src = thumbnail;
+  img.alt = brother.innerText;
+  console.log(img);
   const itemData = {
     sku: brother.innerText,
     name: chosenItem.title,
-    salePrice: finalPrice
+    salePrice: finalPrice,
   };
   const li = createCartItemElement(itemData);
+  li.appendChild(img);
   const list = document.getElementsByTagName('ol');
   list[0].appendChild(li);
   JSON.stringify(saveCartItems(list[0].innerHTML));
@@ -103,12 +112,14 @@ const productList = () => {
     product.addEventListener('click', addItemToCart);
   });
 };
+
 const eventReload = () => {
   const lista = document.getElementsByClassName('cart__item');
   Array.from(lista).forEach((product) => {
     product.addEventListener('click', cartItemClickListener);
   });
 };
+
 const emptyCart = () => {
   const list = document.querySelector('ol');
   list.innerHTML = '';
@@ -117,9 +128,11 @@ const emptyCart = () => {
   total = 0;
   localStorage.setItem('price', 0);
 };
+
 const loading = () => {
   document.getElementById('loader').style.display = 'block';
 };
+
 const loaded = () => {
   document.getElementById('loader').remove();
 };
